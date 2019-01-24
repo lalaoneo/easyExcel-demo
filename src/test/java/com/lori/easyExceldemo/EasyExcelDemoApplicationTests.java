ea621c61120dc31dcf56fc81d9cea513f13805d3
lori <81104174@qq.com>
2019-01-24 23:53:47 +0800

    初始化

---
+++ src/test/java/com/lori/easyExceldemo/EasyExcelDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lori.easyExceldemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EasyExcelDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
